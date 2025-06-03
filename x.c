8cedc8ae5793abccd44487bcf834518833071e34
johndovern <92282293+johndovern@users.noreply.github.com>
2025-06-02 18:39:44 -0700

    up: Reset_title_when_an_empty_title_string_is_give

--- x.c
+++ x.c
@@ -1798,6 +1798,9 @@ xseticontitle(char *p)
 	XTextProperty prop;
 	DEFAULT(p, opt_title);
 
+	if (p[0] == '\0')
+		p = opt_title;
+
 	if (Xutf8TextListToTextProperty(xw.dpy, &p, 1, XUTF8StringStyle,
 	                                &prop) != Success)
 		return;
@@ -1811,6 +1814,9 @@ xsettitle(char *p)
 {
 	XTextProperty prop;
 	DEFAULT(p, opt_title);
+
+	if (p[0] == '\0')
+		p = opt_title;
 
 	if (Xutf8TextListToTextProperty(xw.dpy, &p, 1, XUTF8StringStyle,
 	                                &prop) != Success)
